90795253948153b9297127a60e8cf934ac425fc1
choznerol <b02611002@ntu.edu.tw>
2017-04-06 23:08:24 +0800

    Add 2 options of FB 'like' button

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,12 @@
 <template>
   <section class="footer">
     <span>第二十三屆台大藝術季</span>
+
+    <!-- 風格一 -->
+    <div class="fb-like" data-href="https://www.facebook.com/NTUartfest/" data-layout="standard" data-action="like" data-size="large" data-show-faces="true" data-share="true"></div>
+
+    <!-- 風格二 -->
+    <div class="fb-page" data-href="https://www.facebook.com/NTUartfest/" data-width="340" data-hide-cover="false" data-show-facepile="true"></div>
   </section>
 </template>
 
