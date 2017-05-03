b37b2c7bd22676a2cce4cec809e833fc863d9216
indexhui <indexhui@gmail.com>
2017-05-04 02:03:15 +0800

    Scroll to top vue router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import Router from 'vue-router';
+import VueRouter from 'vue-router';
 import Home from '@/pages/Home';
 import Change from '@/pages/Change';
 import Sponser from '@/pages/Sponser';
@@ -7,39 +7,88 @@ import Academic from '@/pages/Academic';
 import ArtprojectBien from '@/pages/ArtprojectBien';
 import ArtprojectXing from '@/pages/ArtprojectXing';
 
-Vue.use(Router);
+Vue.use(VueRouter);
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home,
-    },
-    {
-      path: '/change',
-      name: 'Change',
-      component: Change,
-    },
-    {
-      path: '/sponser',
-      name: 'Sponser',
-      component: Sponser,
-    },
-    {
-      path: '/academic',
-      name: 'Academic',
-      component: Academic,
-    },
-    {
-      path: '/artproject_bien',
-      name: 'artproject_bien',
-      component: ArtprojectBien,
-    },
-    {
-      path: '/artproject_Xing',
-      name: 'artproject_Xing',
-      component: ArtprojectXing,
-    },
-  ],
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'Home',
+//       component: Home,
+//     },
+//     {
+//       path: '/change',
+//       name: 'Change',
+//       component: Change,
+//     },
+//     {
+//       path: '/sponser',
+//       name: 'Sponser',
+//       component: Sponser,
+//     },
+//     {
+//       path: '/academic',
+//       name: 'Academic',
+//       component: Academic,
+//     },
+//     {
+//       path: '/artproject_bien',
+//       name: 'artproject_bien',
+//       component: ArtprojectBien,
+//     },
+//     {
+//       path: '/artproject_Xing',
+//       name: 'artproject_Xing',
+//       component: ArtprojectXing,
+//     },
+//   ],
+// });
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/change',
+    name: 'Change',
+    component: Change,
+  },
+  {
+    path: '/sponser',
+    name: 'Sponser',
+    component: Sponser,
+  },
+  {
+    path: '/academic',
+    name: 'Academic',
+    component: Academic,
+  },
+  {
+    path: '/artproject_bien',
+    name: 'artproject_bien',
+    component: ArtprojectBien,
+  },
+  {
+    path: '/artproject_Xing',
+    name: 'artproject_Xing',
+    component: ArtprojectXing,
+  },
+];
+
+
+const router = new VueRouter({
+  routes,
+  // scrollBehavior() {
+  //   return window.scrollTo(0, 0);
+  // },
 });
+
+router.beforeEach((to, from, next) => {
+  window.scrollTo(0, 0);
+  next();
+});
+
+export default router;
