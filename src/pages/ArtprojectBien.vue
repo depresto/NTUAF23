cba9d81d3c500731c4a5774ded93fc5e8063996e
Favian <favianchen@gmail.com>
2018-05-02 23:01:47 +0800

    change the content of ArtprojectBien.vue

--- src/pages/ArtprojectBien.vue
+++ src/pages/ArtprojectBien.vue
@@ -2,10 +2,10 @@
   <transition appear name="fade">
   <div
     class="bg"
-    :style="{ 'background-image': 'url(' + bg + ')' }"
   >
     <navBar></navBar>
     <div
+    v-if="false"
       class="hero"
       :style="{ 'background-image': 'url(' + backgroundImage + ')' }"
     >
@@ -13,17 +13,13 @@
     <div class="wrapper">
       <div class="section">
         <div class="intro">
-          <div class="title">變策展部｜理念介紹</div>
-          <div>人意識到自己的方式,常透過外界對自我的感應而形成。而人們所謂的自我,或對
-          於他人的認識,是否常只是經由社會的期待或判斷而生成的、固化的印象單元?透過問題
-          意識的提出、詮釋角度的轉換,我們希望去解構這些關於身分的思維。</div>
-          <div>從此自我認識、行為社會化的反思面向出發,我們試著用不同的藝術媒介去剖析一
-          個群體乃至於個別的人們,在日常生活中究竟是如何地看待或表現自我,而又是否受到了
-          他人觀點的影響,並且以裝置藝術、攝影展、舞劇表演等形式,呈現出我們的觀察。</div>
+          <div class="title">憶策展部｜理念介紹</div>
+          <div>我們每個人都有一個看得見，摸得到的軀殼，然而真正證明我們存在的是沒有形體的記憶。記憶同時是極為個人的也是屬於群體的；它造就我們的情感、思想，卻又稍縱即逝;是身為人的真實紀錄，同時經常是捏造、嫁接、竄改出來的。</div>
+          <div>透過對記憶多重性質的認識與重新詮釋和想像，我們希望去挖掘記憶與人之間，形塑，支配，保存等錯綜複雜又習以為常的關係。</div>
         </div>
         <div class="project-list">
           <mu-row gutter>
-            <mu-col width="100" tablet="100" desktop="100">
+            <mu-col v-if="false" width="100" tablet="100" desktop="100">
 <!--               <div class="fb-video"
               data-href="https://www.facebook.com/NTUartfest/videos/1357235894314831/"
               data-allowfullscreen="true"
@@ -39,41 +35,31 @@
               <div class="list">
                 <li :class="{active:scrollPos == 0}">
                 <a @click="$scrollTo(0)">
-                  【政治正確的精神錯亂】編導式攝影展
+                  【歧憶】
                   </a>
                 </li>
                 <li :class="{active:scrollPos == 1}">
                  <a @click="$scrollTo(1)">
-                 【畫像｜畫相】臉譜展覽與工作坊
+                 【匯憶】
                  </a>
                 </li>
                 <li :class="{active:scrollPos == 2}">
                   <a @click="$scrollTo(2)">
-                  【邃】互動裝置藝術
-                  </a>
-                </li>
-                <li :class="{active:scrollPos == 3}">
-                  <a @click="$scrollTo(3)">
-                  【形意】裝置藝術
+                  【憶測】
                   </a>
                 </li>
               </div>
             </mu-col>
             <mu-col width="100" tablet="50" desktop="33">
               <div class="list">
+                <li :class="{active:scrollPos == 3}">
+                  <a @click="$scrollTo(3)">
+                  【交憶】
+                  </a>
+                </li>
                 <li :class="{active:scrollPos == 4}">
                   <a @click="$scrollTo(4)">
-                  【妖易】實境劇場
-                  </a>
-                </li>
-                <li :class="{active:scrollPos == 5}">
-                  <a @click="$scrollTo(5)">
-                  【烏合】影像創作與行為藝術
-                  </a>
-                </li>
-                <li :class="{active:scrollPos == 6}">
-                  <a @click="$scrollTo(6)">
-                  【WHAT If】舞蹈劇場
+                  【啟憶】
                   </a>
                 </li>
               </div>
@@ -92,21 +78,29 @@
             </mu-col>
             <mu-col width="100" tablet="50" desktop="50">
               <div class="introduce">
-                <div class="title">政治正確的精神錯亂</div>
+                <div class="title">歧憶 Forking Path</div>
                 <div class="discription">
-                  <div class="tag">編導式攝影</div>
                   <div>
-                 民主政體及資本主義的制度下，⼈民作主的美意應是建構在⼈⼈皆有⽴場與中⼼思想的基礎上，然⽽「政治正確」掌握了話語權，因此成為有⼼⼈⼠操弄議題的⼯具。許多⼈為正確而正確，跟⾵盲從，卻對於該議題沒有深⼊探討琢磨，有時甚⾄⽀持者⾼喊的⼝號與其所作所為有所⽭盾，如同「精神錯亂」⼀般卻不⾃知。
+                    在面對同一段群體記憶時，因為不同的生命脈絡，選擇的記憶路徑不盡相同，而產生「歧憶」。
                   </div>
-                   <div>本專案將以編導式攝影的創作形式，藉國族認同及婚姻平權兩大議題作發想，希望藉安排在靜態畫⾯中的細節，表現出目前臺灣社會中普遍被認同的政治正確性事件及其⽭盾。</div>
+                  <div>
+                    記憶就像一部蒙太奇式電影，不限時空的拼貼剪輯。
+                  </div>
+                  <div>
+                    在回憶的過程中，可能會有記憶的變形，包含扭曲、重組、遺忘、重播，但記憶並不會因此變成我們想要的樣子：即便更痛苦或更美好，也已經過竄改而無法辨認。
+                  </div>
+                  <div>
+                    我們想以舞者的肢體結合影像來詮釋群體記憶的異質性。
+                  </div>
+                  <div>
+                    讓觀眾在回憶與不被回憶、理解與不被理解的落差之間，反思如何在人與人記憶的隔閡下自處。
+                  </div>
                   <div class="place">
-                  05.16 - 05.25｜09：00 - 18：00<br/>
-                  05.26｜9：00 - 13：00 <br/>
-                  博雅中央走廊
+                  時間：05.12｜19：00 - 20：00（18:40開始入場）<br/>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  &nbsp;&nbsp;&nbsp;&nbsp;05.13｜15：00 - 16：00、19：00 - 20：00（18:40開始入場）<br/>
+                  地點：博雅館一樓圓桌區
                   </div>
-                  <a class="more" href="https://medium.com/ntuartfest/acad-gender/home">
-                    more
-                  </a>
                 </div>
               </div>
             </mu-col>
@@ -123,18 +117,16 @@
             </mu-col>
             <mu-col width="100" tablet="50" desktop="50">
               <div class="introduce">
-                <div class="title"> 畫像｜畫相</div>
+                <div class="title"> 匯憶</div>
                 <div class="discription">
                   <div>
-                有別於舞台上京劇、崑曲裡各角色的扮相，八家將的臉譜是臺灣民俗與宗教結合的特有產物，每個家將在扮演神明的過程中，為了合其身份，一旦畫上臉譜即開始代表神明，所有的行為舉止皆需特別注意，保持莊嚴肅穆。
+                    「人與人之間，究竟能不能相互理解呢？」台灣目前的社會上存在著很多的分歧、衝突，以及值得需要被探討的議題，而這些困境並非一朝一夕，一個人的力量能夠解決。但我們相信就算不能實際立即地改變現狀，倘若人與人之間能夠多一份瞭解，換位思考，或許就能減少悲傷和衝突發生。
                   </div>
                   <div class="place">
-                  ｜展覽｜<br/>
-                  展期：05.05 - 05.26｜09：00 - 18：00 <br/>
-                  地點：第二學生活動中心3F Global Lounge
-                  ｜工作坊｜<br/>
-                  展期：05.14、05.21 | 13：00 - 15：00<br/>
-                  地點：第二學生活動中心3F Global Lounge
+                  ｜實驗｜<br/>
+                  時間：04.01 - 04.30<br/>
+                  ｜裝置藝術｜<br/>
+                  時間：05.01 - 05.31<br/>
                   </div>
                 </div>
               </div>
@@ -152,22 +144,18 @@
             </mu-col>
             <mu-col width="100" tablet="50" desktop="50">
               <div class="introduce">
-                <div class="title">邃</div>
+                <div class="title">憶測</div>
                 <div class="discription">
                   <div>
-                 本次展覽以「一個讓人意識到自我存在的窗口」為理念，利用外在空間的劃分及變形，擺設可與觀展者互動的裝置，使觀展者在走入進設計巧思的空間時，暫時剝離層層非自我主體的外衣。讓過去與當下糾纏不定的心靈狀態，外在眼光及個人價值的拉扯，形成辨證與肯定的過程，在無數光陰的縫隙間更加認識自我的存在。
+                    「眼連著腦，腦連著神經系統。這系統瞬息之間就把訊息傳遞給往昔的回憶及當前的感受。」— 吳爾芙，Three Guineas
+                  </div>
+                  <div>
+                 科學家認為，記憶的生產牽涉了複雜的腦內運動，是大量的化學反應。即使如此，其仍然無法解釋記憶檔案之間交融與錯置的可能。憶測，即是對個體的記憶進行測驗，文本間編碼與指涉的互文性，誘發觀者生產自身的價值取向。在這裡，意義的生產是文化與政治的碰撞、是真實與幻象的戰役、是個體與機構的對抗。
                   </div>
                   <div class="place">
-                  ｜互動式裝置藝術 ｜<br/>
-                  展期： 05.08 - 05.26 平日｜12：00 - 20：00  <br/>
-                  地點：社科院外廊道<br/>
-<!--                   ｜紀實文字作品｜<br/>
-                  展期：05.09 - 05.26｜00：00 - 23：59<br/>
-                  地點：社科院外走廊 -->
+                  時間： 05.07 - 05.25｜09:00 - 18:00<br/>
+                  地點：第一活動中心 水池/植物走廊<br/>
                   </div>
-<!--                   <a class="more" href="https://medium.com/ntuartfest/acad-gender/home">
-                    more
-                  </a> -->
                 </div>
               </div>
             </mu-col>
@@ -184,18 +172,20 @@
             </mu-col>
             <mu-col width="100" tablet="50" desktop="50">
               <div class="introduce">
-                <div class="title">形意</div>
+                <div class="title">交憶</div>
                 <div class="discription">
                   <div>
-                  藝術或是創作這件事，該有一個可批判的標準嗎？
+                  交，縱橫錯雜之意，如同理還亂的記憶與情緒。假使那些層層疊疊的「東西」無法被切片，那麼以不同的角度去翻轉與再定義，是否因而有趣？
                   </div>
                   <div>
-                  由八位創作者共同創作三幅立體作品，使用各種複合媒材（包含油漆、顏料、木材、金屬、燈光等）在畫上添加各自的痕跡，創作者不賦予作品背後的概念或是想法，而是由觀看者去賦予它意義。藉由作品闡述權力的移轉，模糊創作者與觀看者之間的界線，創造觀眾與創作者身分之間的轉換與對話。
+                  誠摯邀請每一個複雜的靈魂在此展開記憶的交疊，而如果幸運，我們或許不經意複寫彼此的之前，或以眼神交會共同盲猜當下，或將自己的在場以某種證明遺留之後。
+                  </div>
+                  <div>
+                  這是赤裸的交易、不負責任的交流以及......隱晦的交心。所有動作在此都微小而重要，因為他們將成為無法再與前一秒相同的，共同的記憶之作。
                   </div>
                   <div class="place">
-                  ｜展覽｜<br/>
-                  時間：05.05 - 05.28｜09：00 - 18：00 <br/>
-                  地點：霖澤館、模擬法庭間廣場
+                  時間：05.07 - 05.18｜10:00 - 20:00 <br/>
+                  地點：第一活動中心 104展室
                   </div>
                 </div>
               </div>
@@ -213,84 +203,13 @@
             </mu-col>
             <mu-col width="100" tablet="50" desktop="50">
               <div class="introduce">
-                <div class="title">妖易</div>
+                <div class="title">啟憶</div>
                 <div class="discription">
-                  <div>都市中有許多的妖魔鬼怪，長相與人並無差異，混在人群中與人類共同生活。</div>
-                  <div>
-                  關於妖怪的資訊人們多由口耳相傳得知，再以口耳相傳告知。久而久之，就連原先的妖怪們聽到關於自己的傳說，都還以為是都市中新來了一批比自己更可怕的妖怪。妖怪究竟是原本就存在於世界上的生物，還是人類訛言下的產物？
+                  <div>現實跟記憶往往有著一線之隔，並不是隨時都有辦法輕易切換，可是在某些瞬間進入記憶卻是相當容易的，甚至是不可避免的陷入其中，因此，這個專案希望藉由透過聲音、影像來喚起記憶，可能是日常周遭的聲音，可能是一首熟悉的歌，可能是某些似曾相識的畫面，讓觀者能對進入記憶的瞬間產生共鳴，有更多不同的想法，再次開啟進入記憶廊道的開關。
                   </div>
                   <div class="place">
-<!--                   ｜裝置作品｜<br/> -->
-                  時間：05.13 - 05.14｜16:30~19:30  <br/>
-                  地點：社科院 3F 學習開放空間 & 西側桌椅區<br/>
-<!--                   ｜校園導覽｜<br/> -->
-                  時間：05.20 - 05.21｜16:30~19:30  <br/>
-                  地點：第一學生活動中心 B1 文藝展示室
-                  </div>
-                </div>
-              </div>
-            </mu-col>
-          </mu-row>
-        </div>
-        <div class="project">
-          <mu-row gutter>
-            <mu-col width="100" tablet="50" desktop="50">
-              <div
-              class="project-img"
-              :style="{ 'background-image': 'url(' + crow + ')' }"
-              >
-              </div>
-            </mu-col>
-            <mu-col width="100" tablet="50" desktop="50">
-              <div class="introduce">
-                <div class="title">烏合</div>
-                <div class="discription">
-                  <div>
-                  面對社會所形塑出的主流價值，為了融入人群、抑或是為了更加突出，人們是否會將自我變形……？
-                  </div>
-                  <br/>
-                  <div>影像創作</div>
-                  <div>｜從眾效應｜05.15 - 05.17 ｜09：00 - 18：00 活大104</div>
-                  <div>從動畫裡看到最真實的人性，一窺從眾化的樂觀面與悲觀面。</div>
-                  <div>｜標籤化｜05.18 - 05.20｜09：00 - 18：00 活大104</div>
-                  <div>以訪談的形式對身體自主權展開一系列辯論。</div>
-                  <div>｜網路暴力｜05.21 - 05.22｜09：00 - 18：00 活大104</div>
-                  <div>五分鐘的紀錄片帶你走進受害者眼裡的世界。</div>
-                  <div>行為藝術</div>
-                  <div>｜裸｜05.15 - 05.19｜12：30 活大104</div>
-                  <div>人們是否還願意脫下網路世界的保護膜，以最赤裸的狀態面對他人的注視與評論？</div>
-                </div>
-              </div>
-            </mu-col>
-          </mu-row>
-        </div>
-        <div class="project">
-          <mu-row gutter>
-            <mu-col width="100" tablet="50" desktop="50">
-              <div
-              class="project-img"
-              :style="{ 'background-image': 'url(' + whatif + ')' }"
-              >
-              </div>
-            </mu-col>
-            <mu-col width="100" tablet="50" desktop="50">
-              <div class="introduce">
-                <div class="title">What If</div>
-                <div class="discription">
-                  <div>身份及角色隨著時空不斷變形。</div>
-                  <div>
-                  「What If」以街舞舞蹈劇場形式呈現一故事劇情，舞台的設計環繞著觀者，讓觀眾仿佛身在表演的劇情之中伴隨著舞者體驗著各種抉擇以及之後的結果，藉以表達人在生命當中，時常面臨看似稀鬆平常的選擇，卻往往能深遠地影響著你我。
-                  </div>
-                  <a href="https://www.facebook.com/events/433819283640359/">【WHAT If】舞蹈劇場</a>
-                  <a href="https://ntuaf23.backme.tw/shops/515?checked=true&locale=zh-TW">前往購票</a>
-                  <div class="place">
-                  ｜舞蹈劇場｜<br/>
-                  場次及地點：  <br/>
-                  05.26｜19:30 - 20:30<br/>
-                  05.27｜14:30 - 15:30<br/>
-                  05.27｜19:30 - 20:30<br/>
-                  05.28｜14:30 - 15:30<br/>
-                  地點：第一學生活動中心B1 文藝展示室
+                  時間：05.07 - 05.11、05.14 - 05.18 <br/>
+                  地點：社會科學院 1F大廳<br/>
                   </div>
                 </div>
               </div>
