69b6ad7da344e30fe594439e6e74cfd3f49a5cc5
Favian <favianchen@gmail.com>
2018-05-02 23:19:06 +0800

    change the content of ArtprojectXing.vue

--- src/pages/ArtprojectXing.vue
+++ src/pages/ArtprojectXing.vue
@@ -2,7 +2,7 @@
   <transition appear name="fade">
   <div>
     <navBar></navBar>
-    <div
+    <div v-if="false"
       class="hero"
       :style="{ 'background-image': 'url(' + backgroundImage + ')' }"
     >
@@ -11,13 +11,13 @@
     <div class="wrapper">
       <div class="section">
         <div class="intro">
-          <div class="title">形策展｜理念介紹</div>
-          <div>科技帶領世界漸漸地改變，將人們的深信不疑轉換為下一個迷思；驀然回首，世界早已不是當初的模樣。</div>
-          <div>《形》刺激人類做夢與想望的能力，於台大校園的平行世界中，劃分虛構的極端聚落，猶如科技變形的實驗場，展開未來世界的可能維度。人性在〈懼形〉泯滅、在〈制形〉消長、在〈探形〉超越。這是一場架空思想而置身其境的旅程，從多元媒介透析觀者的自身常識與科技認知，在不同層次挑戰價值體系並批判運作本質，藉以反思當代科技走向，形塑個人－社會複合體（Individual-Social Complex）的另類（alternative）想像。</div>
+          <div class="title">常策展部｜理念介紹</div>
+          <div>常是一種自然，是毫無意識的累積、是日復一日的循環。我們總是週期性地失落、習慣性地微笑，在一成不變的日子裡練習生活，學會世界上不言而喻的規則，然後安然地，隱身其中。</div>
+          <div>《希望透過與場域與身分的對話，對質日常裡的秩序與荒謬，重新看待自身存在的環境與角色，也許適應，也許破解。日常本就是那些看似永恆不變，卻又難以掌握的。</div>
         </div>
         <div class="project-list">
           <mu-row gutter>
-            <mu-col width="100" tablet="50" desktop="100">
+            <mu-col v-if="false" width="100" tablet="50" desktop="100">
 <!--               <div class="fb-video"
               data-href="https://www.facebook.com/NTUartfest/videos/1357235894314831/"
               data-allowfullscreen="true"
@@ -33,22 +33,27 @@
               <div class="list">
                 <li :class="{active:scrollPos == 0}">
                 <a @click="$scrollTo(0)">
-                  世界入口
+                  局外人 Outsider
                   </a>
                 </li>
                 <li :class="{active:scrollPos == 1}">
                  <a @click="$scrollTo(1)">
-                  制形
+                  常·視
                  </a>
                 </li>
                 <li :class="{active:scrollPos == 2}">
                   <a @click="$scrollTo(2)">
-                  懼形
+                  觀 LOG IN
                   </a>
                 </li>
                 <li :class="{active:scrollPos == 3}">
                   <a @click="$scrollTo(3)">
-                 探形
+                 動木園．ANIMOOD
+                  </a>
+                </li>
+                <li :class="{active:scrollPos == 4}">
+                  <a @click="$scrollTo(4)">
+                 日日 Shadows of Day
                   </a>
                 </li>
               </div>
@@ -67,20 +72,16 @@
             </mu-col>
             <mu-col width="100" tablet="50" desktop="50">
               <div>
-                <div class="title">世界入口</div>
+                <div class="title">局外人 Outsider</div>
                 <div class="discription">
-                  <div>結構形變．重組可能</div>
-                  <br/>
+                  <div>本專案旨在打破慣性日常，誇大多數人以為的反日常行為。</div>
                   <div>
-                 「我們對於世界的理解，就猶如呼吸吐納般的自然。」
+                 現代人總是能夠輕易地將不舒適的狀態予以隔絕，像是戴上耳機，使自身平行於外在嘈雜世界；輕易低頭，用科技化解面對面的尷尬；抑或是自我說服，將責任交付他人而拒絕承擔。
                   </div>
-                   <div>〈世界入口〉所要提供的，並非是如蟲洞（Wormhole）的時空穿越感，而是在觀看的過程中釋義多元價值並存的豐富態樣。透過結構的不斷翻轉，產生層次上裂解與重組的語彙，同時間衝擊僵化的當今世界。藉由建立起塑造、拆解與還原的循環疊合，引領觀者在潛意識內探索超越現在時空的可能性，作為「變形」的藝術實踐。</div>
+                   <div>希望透過行為藝術的展現，試著使觀眾習慣潛意識裡認知的反日常行為、不正常狀態，不再以局外人角度去看待他人日常。</div>
                   <div class="place">
-                  05.08 - 05.26｜10：00 - 18：00 臺大社會科學院介宙館穿堂<br/>
+                  時間：05.04｜10:00 - 13:00<br/>
                   </div>
-<!--                   <a class="more" href="https://medium.com/ntuartfest/acad-gender/home">
-                    more
-                  </a> -->
                 </div>
               </div>
             </mu-col>
@@ -97,27 +98,47 @@
             </mu-col>
             <mu-col width="100" tablet="50" desktop="50">
               <div>
-                <div class="title">制形</div>
+                <div class="title">常·視</div>
                 <div class="discription">
-                  <div>數據制約．自由瓦解</div>
-                  <br/>
+                  <div>在日常的隧道中，來來往往的人看到的風景不盡相同。</div>
                   <div>
-                數據所形成的社會秩序，如同程式碼般精準運行。一舉一動受到演算法監控，是最佳化的理想，亦或是自由選擇的終結？科技帶來的權力流動與物質上的富足，使人盲目渴望掌握，進而反噬人類最根本的能動性。〈制形〉的創作藉由顛覆對既有秩序的想像，探討科技與人類相互牽制的內在關係。
+                    人生中的事情總是一體兩面的，一道光灑下，我們看到的究竟是明面或是暗面？透過存在你我周遭的環污議題，我們呈現給觀眾相異的立場，闡述在不同的觀點和想法下，即使雙方遇到完全相同的情況，也會產生南轅北轍的日常。
+                  </div>
+                  <div>
+                    「常視」，常即日常，視並非指物理上的看，而是指人的視角和觀點。另外，取其諧音「常識」，每個人自認為的常識都不同，也凸顯「相異」的理念。
                   </div>
                   <div class="place">
-                  ｜現代舞蹈｜<br/>
-                  現代舞蹈《制形》以開放式舞台解放群眾，帶領您一同體驗資訊新秩序下的運算重啟。<br/>
-                  展期：05.14｜19：00、21：00  <br/>
-                  地點：臺大博雅教學館醉月湖側平台
-                  <div>
-                    <a class="more" href="https://medium.com/ntuartfest/acad-gender/home">
-                      前往報名
-                    </a>
+                  時間：05.07 - 05.15｜09:00 - 21:00  <br/>
                   </div>
-                  <br/>
-                  ｜互動裝置藝術｜<br/>
-                  展期：05.15 - 05.26｜09：00 - 17：00 <br/>
-                  地點：臺大博雅教學館101教室外
+                </div>
+              </div>
+            </mu-col>
+          </mu-row>
+        </div>
+        <div class="project">
+          <mu-row gutter>
+            <mu-col width="100" tablet="50" desktop="50">
+              <div
+              class="project-img"
+              :style="{ 'background-image': 'url(' + dance + ')' }"
+              >
+              </div>
+            </mu-col>
+            <mu-col width="100" tablet="50" desktop="50">
+              <div>
+                <div class="title">觀 LOG IN</div>
+                <div class="discription">
+                  <div>
+                    隨著網路世代的崛起，諸多作為連結虛擬與現實橋樑的社群網站、輿論平台逐漸蠶食著我們以現實為基調的日常生活。</div>
+                  <div>
+                    我們將以實境劇場的形式，藉由不同空間之間的轉換，呈現出現實世界與虛擬世界是如何密切關聯，且難以切割，更讓現場觀眾走入舞台，成為演出的一部分。
+                  </div>
+                  <div>
+                    此外，我們亦將演出空間自現實端延伸至虛擬端—正式演出前於網路討論區發表與劇情密切關聯之楔子，以及正式演出時透過直播平台同步放送演出內容。鄉民的推噓文以及聊天室的回應也會呈現於劇情中，甚至可能影響劇情走向⋯⋯。
+                  </div>
+                  <div class="place">
+                  時間：05.11｜18:30 - 21:30、05.12｜16:30 - 20:00、05.13｜16:30 - 20:00  <br/>
+                  地點：第一活動中心 地下室
                   </div>
                 </div>
               </div>
@@ -135,34 +156,15 @@
             </mu-col>
             <mu-col width="100" tablet="50" desktop="50">
               <div>
-                <div class="title">懼形</div>
+                <div class="title">動木園．ANIMOOD</div>
                 <div class="discription">
-                  <div>科技崇拜．恐懼懷疑</div>
-                  <br/>
                   <div>
-                 科技取代人類成為了世界的支配者，而人類成為大自然中緩慢演化的落後產物。當科技成為唯一的救贖，退化後的我們，只能用力膜拜、虔誠信仰，祈求「祂」能夠繼續用不自然的色光控制那早已無所謂的自身感覺與意識。
-〈懼形〉以宗教的形式，藉由劇場及裝置，將科技形塑為總體意志的延伸，直往心中對於個體價值的質疑與恐懼。
+                 本專案是以裝置藝術的形式在臺大放置動物木板藝術，並使用青苔塗鴉，讓它隨著時間長出綠意，賦予裝置藝術生命，讓它能真正地與人們「活」在一起、融入臺大，成為日常的一角；並在展出過程中藉由每天在不同角度、不同時辰所拍下的照片，見證青苔的生長、時間的流轉，讓觀眾去反思，究竟何為「常」，又其真如我們直覺所想的，一成不變嗎？
                   </div>
                   <div class="place">
-                  ｜劇場。羅拔 Paradise City｜<br/>
-                  展期：
-05.07｜15：00、19：00
-05.08｜19：00
-05.09｜19：00<br/>
-                  <div>
-                    <a class="more" href="https://medium.com/ntuartfest/acad-gender/home">
-                      前往報名
-                    </a>
-                  <br/>
+                  時間：3月 - 5月<br/>
+                  地點：鹿鳴草皮、振興草皮
                   </div>
-                  地點：第一學生活動中心 B1文藝展示室<br/>
-                  ｜裝置藝術｜<br/>
-                  展期：05.08 - 05.26｜10：00 - 18：00<br/>
-                  地點：第一學生活動中心 103
-                  </div>
-<!--                   <a class="more" href="https://medium.com/ntuartfest/acad-gender/home">
-                    more
-                  </a> -->
                 </div>
               </div>
             </mu-col>
@@ -179,31 +181,17 @@
             </mu-col>
             <mu-col width="100" tablet="50" desktop="50">
               <div>
-                <div class="title">探形</div>
+                <div class="title">日日 Shadows of Day</div>
                 <div class="discription">
                   <div>
-                  人類和機械之間的界線日益模糊；科技化後的軀體形成「賽伯格人」 (Cyborg)，感官大幅擴增，改變了我們感知世界的方式，自由選擇與機械結合與否的同時，亦重新選擇雙目所見、雙耳所聞。屆時，何謂真實？又或者無所謂真實？
+                  日，是人們私自將時間以太陽的軌跡定義的最小單位。
                   </div>
                   <div>
-                  我們將如何生活？在重新定義環境之際，是不是也將重新定義自己？每個人心中自有一套世界運作的系統，有些相容並存，有些矛盾互斥，人類破碎成遊牧群體。過去天擇演化決定了物種的分支，這一次是人擇進化，個體之間從此形態各異。
+                  日日，是三段以光的角度講述時間的詩，重複播放的今日，假扮今日的昨日，以及尚未構築的明日，而今日又拖曳著昨日，跨進明日的洞裏。
                   </div>
                   <div class="place">
-                  ｜互動裝置藝術｜<br/>
-                  眼前景物經過扭曲、加工、再製，才被人們接收，世界遭科技重置，見亦非見。我們曾經說的「眼見為憑」還適用嗎？我們以科技的角度重新凝視這個世界，在感覺與認知之間的巨大落差下，提供觀者感知體驗的無限可能。<br/>
-                  時間：05.08 - 05.26｜10：00 - 18：00 <br/>
-                  地點：華南銀行臺大分行
-                  </div>
-                  <div class="place">
-                  ｜耳機共感體驗｜<br/>
-                  「人類是否能有超乎語言、文字的溝通可能？」
-賽伯格人推翻文字或言語的交流，訊息與的交流將透過樂音完成。聲音串連人們的感官，感覺本體在纜線之中流竄擴散，生理脈動於震動之間感應回饋。耳機共感體驗讓聽者群聚連線空間，體驗共同想像建構的廣大感官世界。<br/>
-                  時間：05.12、05.19、05.26｜18:30 - 19：00、19：30 - 20：00 <br/>
-                  地點：華南銀行臺大分行
-                  </div>
-                    <div>
-                    <a class="more" href="https://medium.com/ntuartfest/acad-gender/home">
-                      前往報名
-                    </a>
+                  時間：05.18｜19：00 - 20：00、05.19｜19：00 - 20：00<br/>
+                  地點：舊體育館 2F桌球室
                   </div>
                 </div>
               </div>
